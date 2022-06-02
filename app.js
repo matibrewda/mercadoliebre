1991657654f399434be23a76302c55329c8392df
Matias <matia@LAPTOP-HUGO76BI>
2022-06-02 16:53:30 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const res = require('express/lib/response');
+const path = require('path')
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+app.get('/registro' , (req,res)=>{
+    res.sendFile(path.join(__dirname, './views/registro.html'))
+});
+
+app.listen(3030, () => console.log("Servidor"));
+
+app.use(express.static('public'));
